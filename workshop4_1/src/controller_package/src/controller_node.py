2a99241967855bf3e8c0de841e3329e3b3a753e1
Iliyan Teofilov <iliyanvt18@gmail.com>
2025-05-14 08:28:45 +0000

    Defined new message type and started implementing controller node

--- workshop4_1/src/controller_package/src/controller_node.py
+++ workshop4_1/src/controller_package/src/controller_node.py
@@ -1,62 +1,42 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float64
 import time
 
+from controller_package.msg import MovementRequest
 
-class Node:
+
+class ControllerNode:
     def __init__(self, node_name):
         self.initialized = False
         self.node_name = node_name
-        rospy.loginfo("Initializing ROS node...")
+        rospy.loginfo("Initializing controller node...")
         rospy.init_node(self.node_name, anonymous=True)
-
-        # Construct subscribers
-        self.subscriber = rospy.Subscriber(
-            "/endpoint",
-            Float64,
-            self.subscriber_cb,
-            #Change buff size and queue size accordingly
-            buff_size=1000000,
-            queue_size=1,
-        )
 
         # Construct publishers
         self.publisher = rospy.Publisher(
-            "/endpoint",
-            Float64,
+            "/motors",
+            MovementRequest,
             #Change buff size and queue size accordingly
-            queue_size=1,
+            queue_size=10,
         )
 
-        self.recvSequence = 0
         self.sendSequence = 0
 
         self.initialized = True
-        rospy.loginfo("Node initialized!")
-        self.timer = rospy.Timer(rospy.Duration(1.0), self.publish_time)
+        rospy.loginfo("Controller node initialized!")
 
+        while True:
+            action = input("desired action: ")
+            distance = input("distance: ")
+            unit = input("unit: ")
 
-    def subscriber_cb(self, data):
-        if not self.initialized:
-            return
-        
-        self.recvSequence += 1
-        rospy.loginfo("Received message ({seqNumber}): {payload}".format(seqNumber=self.recvSequence,payload=data.data))
+            print(action, distance, unit)
     
-    def publish_time(self, event):
-        msg = Float64()
-        msg.data = time.time()
-
-        self.publisher.publish(msg)
-        self.sendSequence += 1
-        
-        rospy.loginfo("Message sent (({seqNumber})): {payload}".format(seqNumber=self.sendSequence,payload=msg))
 
 if __name__ == "__main__":
     try:
-        camera_node = Node(node_name = "template_node")
+        camera_node = Node(node_name = "controller_node")
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
