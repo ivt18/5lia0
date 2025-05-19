e0da069d4a96c168003bef3d4721149e23d1f043
Adrien Verriele <adrien.verriele08@gmail.com>
2025-05-19 13:28:38 +0200

    implemented tracker motion

--- workshop4_1710303/src/controller_package/src/controller_node.py
+++ workshop4_1710303/src/controller_package/src/controller_node.py
@@ -4,7 +4,9 @@ import time
 
 import numpy as np
 import rospy
+
 from controller_package.msg import MovementRequest
+from jetson_camera.msg import ObjectPosition
 
 
 class ControllerNode:
@@ -21,6 +23,13 @@ class ControllerNode:
             queue_size=10,
         )
 
+        self.subscriber = rospy.subscriber(
+            "/camera/object_position",
+            ObjectPosition,
+            self.track_object,
+            queue_size = 1
+        )
+
         self.initialized = True
 
         rospy.loginfo("Controller node initialized!")
@@ -34,6 +43,30 @@ class ControllerNode:
             msg.request_type = request_type
             msg.value = value
             self.publisher.publish(msg)
+        
+
+    track_object(self, object_position):
+        kp = 0.1
+        center_x = object_position.image_width/2
+        object_x = object_position.x + object_position.width/2
+        error_x = object_x - center_x
+        
+        if error_x < 30:
+            msg_move = MovementRequest()
+            msg_move.request_type = 0
+            msg_move.value = 0.5
+            self.publisher.publish(msh_move)
+        else:
+        
+            angle = kp * error_x
+            angle = max(min(angle, 30), -30)
+            
+            msg_turn = MovementRequest()
+            msg_turn.request_type = 1
+            msg_turn.value = 2 * math.pi * angle / 360
+            self.publisher.publish(msg_turn)
+        return
+
         
 
 if __name__ == "__main__":
