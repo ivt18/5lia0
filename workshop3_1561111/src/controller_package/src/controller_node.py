feff4933568c9bd71a2ea0ee8f33f55c6c4b6989
Stijn <6910589+njitsh@users.noreply.github.com>
2025-05-21 17:10:19 +0200

    Add initial QR code control

--- workshop3_1561111/src/controller_package/src/controller_node.py
+++ workshop3_1561111/src/controller_package/src/controller_node.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import rospy
 from controller_package.msg import MovementRequest
+from jetson_camera.msg import QRCodes
 
 
 class ControllerNode:
@@ -21,6 +22,14 @@ class ControllerNode:
             queue_size=10,
         )
 
+        # Construct subscriber
+        self.sub_qr_code = rospy.Subscriber(
+            "/camera/qr_code",
+            QRCodes,
+            self.qr_code_cb,
+            queue_size=10
+        )
+
         self.initialized = True
 
         rospy.loginfo("Controller node initialized!")
@@ -34,7 +43,23 @@ class ControllerNode:
             msg.request_type = request_type
             msg.value = value
             self.publisher.publish(msg)
-        
+
+    def qr_code_cb(self, data):
+        if not self.initialized:
+            return
+
+        # Process the QR code data
+        if data.found:
+            msg = MovementRequest()
+            msg.request_type = 1
+            msg.value = 1
+            self.publisher.publish(msg)
+
+            rospy.loginfo("Found QR code: %s", data.data)
+
+            # Here you can add logic to handle the QR code data
+            # For example, you can send a command to the motors based on the QR code data
+
 
 if __name__ == "__main__":
     try:
