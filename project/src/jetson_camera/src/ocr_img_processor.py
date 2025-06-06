897c8c470e700885c159c9c09822e407cc093d36
vlad <vlad.plaiasu.vp@gmail.com>
2025-06-06 13:49:24 +0100

    allow selected cropping for ocr detection

--- project/src/jetson_camera/src/ocr_img_processor.py
+++ project/src/jetson_camera/src/ocr_img_processor.py
@@ -99,6 +99,7 @@ class OcrCompressedNode:
             rospy.signal_shutdown("Failed to connect to OCR server.")
             return
 
+        self.s = self.setup_connection()
         self.message_rate = rospy.Rate(10)
 
         self.commands_history = CommandHistory()
